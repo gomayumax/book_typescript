2b6cc5ccf6c4adbe00f798116d3bf57e5dfd7a2f
ごま <gomayumax@users.noreply.github.com>
2021-08-25 00:01:26 +0900

    型推論

--- src/sample3.ts
+++ src/sample3.ts
@@ -1,6 +1,16 @@
-function expo2() {
-    return amount ** 2
-}
-console.log(expo2(1000))
-console.log(expo2('1000')) // error
+import UsersJson from './users.json'
+interface User {
+    id: number
+    created_at: string
+    profile: {
+        name: {
+            first: string
+            last: string
+        },
+        age: number
+        gender: string
+        enabled: boolean
+    }
 
+}
+type Users = User[]
