70b36053e2d04b410117607735bae42744f08b37
ごま <gomayumax@users.noreply.github.com>
2021-08-30 11:59:04 +0900

    ガード節

--- src/sample4.ts
+++ src/sample4.ts
@@ -99,3 +99,86 @@ type User4 = {
 }
 
 const tuple = [false, 1, '2'] as const // readonly
+
+// 4-3
+// typeof でガード節
+function reset(value: number | string | boolean) {
+    const v0 = value
+    if (typeof value === 'number') {
+        const v1 = value
+        return 0
+    }
+
+    if (typeof value === 'string') {
+        const v2 = value
+        return ''
+    }
+
+    const v3 = value
+    return false
+}
+// プロパティのチェック(in)でガード節
+type UserGender = {gender: string; [k:string]: any}
+type UserA = UserGender & {name: string}
+type UserB = UserGender & {age: number; graduate: string}
+function judgeUserType(user: UserA | UserB) {
+    if ('gender' in user) {
+        const u0 = user
+        console.log('user type is UserA | UserB')
+    }
+
+    if ('name' in user) {
+        const u1 = user
+        console.log('user type is UserA')
+        return
+    }
+
+    const u2 = user
+    console.log('user type is UserB')
+}
+
+// instanceでガード節
+class Creature {
+    breathe() {}
+}
+class Animal extends Creature {
+    shakeTail() {}
+}
+class Human extends Creature {
+    greet () {}
+}
+function action(creature: Animal | Human | Creature) {
+    const c0 = creature
+    c0.breathe()
+    if (creature instanceof Animal) {
+        const c1 = creature
+        return c1.shakeTail()
+    }
+    if (creature instanceof Human) {
+        const c2 = creature
+        return c2.greet()
+    }
+
+    const c3 =  creature
+    return c3.breathe()
+}
+
+// ユーザ定義のtypeでガード節
+function isUserA(user: UserA | UserB): user is UserA {
+    return user.name !== undefined
+}
+function isUserB(user: UserA | UserB): user is UserB {
+    return user.age !== undefined
+}
+function getUserType(user: any) {
+    const u0 = user
+    if (isUserA(u0)) {
+        const u1 = u0
+        return 'A'
+    }
+    if (isUserB(u0)) {
+        return 'B'
+    }
+    return 'unknown'
+}
+const x = getUserType({name: 'Taro'})
