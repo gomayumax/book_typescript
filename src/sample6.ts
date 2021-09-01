9d847ca2b1ce53daabf08e4bf2e5a96a609344fe
ごま <gomayumax@users.noreply.github.com>
2021-09-02 00:13:24 +0900

    型の抽出

--- src/sample6.ts
+++ src/sample6.ts
@@ -74,3 +74,17 @@ type IsType<T, U>={
 type IsString2 = IsType<Properties, string>
 type IsNumber = IsType<Properties, number>
 type IsBoolean = IsType<Properties, boolean>
+
+interface Properties {
+    name: string
+    age: number
+    walk: () => void
+    jump: () => Promise<void>
+}
+type Filter<T, U> = {
+    [K in keyof T]: T[K] extends U ? K : never
+}[keyof T]
+
+type StringKeys = Filter<Properties, string>
+type NumberKeys = Filter<Properties, number>
+type FunctionKeys = Filter<Properties, Function>
