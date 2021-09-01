43beb83f5050398aef3ac54d436c20c11060a295
ごま <gomayumax@users.noreply.github.com>
2021-09-01 22:49:57 +0900

    関数のGenerics

--- src/sample6.ts
+++ src/sample6.ts
@@ -20,3 +20,18 @@ const box13: Box3<string> = {value: 'text'}
 const box23: Box3<number> = {value: 2}
 const box33: Box3<boolean> = {value: true} // error
 
+function boxed<T>(props: T) {
+    return {value: props}
+}
+const box00 = boxed('text') // Genericsの省略
+const box01 = boxed(false as boolean | null)
+const box001 = boxed<string | null >(null) // アサーションによる型の付与
+interface Props {
+    amount: number
+}
+function boxed2<T extends Props>(props: T) {
+    return {value: props.amount.toFixed(1)}
+}
+const c022 = boxed2({amount: 2})
+const c023 = boxed2({value: 0}) // error
+const c024 = boxed2({amount: 'test'}) // error
