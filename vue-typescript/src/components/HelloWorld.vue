4052c329ce2ec4fc971bb14f58fcb5adf337f5ce
ごま <gomayumax@users.noreply.github.com>
2021-09-04 15:55:28 +0900

    型を満たすcomputed型

--- vue-typescript/src/components/HelloWorld.vue
+++ vue-typescript/src/components/HelloWorld.vue
@@ -33,6 +33,14 @@
 <script lang="ts">
   import Vue, {PropType} from "vue"
 
+  export type Rect = {
+    width: number
+    height: number
+  }
+  export type Position = {
+    top: number
+    left: number
+  }
 export default Vue.extend({
   name: 'HelloWorld',
   props: { // props の型宣言
@@ -50,6 +58,14 @@ export default Vue.extend({
       type: Array as PropType<{ task: string }[]>
     }
   },
+  data: () => ({
+    bounds: null as Clientrect | DOMRect | null,
+  }),
+  methods: {
+    onClickElement({target}: {target: HTMLElement}): void {
+      this.bounds = target.getBoundingClientRect()
+    }
+  },
   computed: {
     message(): string{
       return this.msg
@@ -59,6 +75,24 @@ export default Vue.extend({
     },
     myFirstTask(): string{
       return this.arr[0].task
+    },
+    boundsInfo(): string {
+      if (this.bounds === null) return ""
+      return JSON.stringify(this.bounds)
+    },
+    rect(): Rect {
+      if( this.bounds === null) return {width: 0, height: 0}
+      return {
+        width: this.bounds.width,
+        height: this.bounds.height
+      }
+    },
+    position(): Position {
+      if(this.bounds === null) return {top: 0, left: 0}
+      return {
+        top: this.bounds.top,
+        left: this.bounds.left
+      }
     }
   }
 })
