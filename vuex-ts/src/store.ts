3f07a7de6cd407cdf197f225f1ea33a3db774978
ごま <gomayumax@users.noreply.github.com>
2021-09-04 22:44:39 +0900

    実際に書いてみたVuex(問題あり

--- vuex-ts/src/store.ts
+++ vuex-ts/src/store.ts
@@ -5,12 +5,41 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-
+        count: 0 as number | null,
+        name: null as string | null
+    },
+    getters: {
+        getName(state, getters) {
+            return state.name
+        },
+        greet(state, getters) {
+            return `My name is ${getters.getName.toUpperCase()}`
+        }
     },
     mutations: {
-
+        setName(state, payload) {
+            state.name = payload
+        },
+        increment(state) {
+            state.count++ // error nullの可能性
+        }
     },
     actions: {
-
+        asyncSetName(ctx, payload) {
+            ctx.commit('setName', {name: payload})
+            console.log(ctx.state.name)
+        },
+        asyncIncrement(ctx) {
+            ctx.commit('increment')
+            console.log(ctx.state.count)
+        },
+        async countUp(ctx) {
+            while (true) {
+                await (() => new Promise(resolve => {
+                    setTimeout(resolve, 1000)
+                }))()
+                ctx.dispatch('increment')
+            }
+        }
     }
 })
