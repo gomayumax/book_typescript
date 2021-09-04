f59c8e337c4f996121c7ffa3e0b63496afed6f40
ごま <gomayumax@users.noreply.github.com>
2021-09-04 22:10:26 +0900

    install vue-property-decorator

--- vue-class-component/src/App.vue
+++ vue-class-component/src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TS App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import {Component, Vue} from "vue-property-decorator"
+  import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  @Component({
+    components: {
+      HelloWorld
+    },
+  })
+  export default class App extends Vue {}
 </script>
 
 <style>
