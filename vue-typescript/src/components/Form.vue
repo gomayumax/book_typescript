2908656570ca5c2b813e6768a64c3503cd2b4636
ごま <gomayumax@users.noreply.github.com>
2021-09-04 15:44:13 +0900

    computed の型

---
+++ vue-typescript/src/components/Form.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+<script lang="ts">
+    import Vue from "vue"
+  export default Vue.extend({
+    name: "Form",
+      data: () => ({
+          defaultDateLabel: "1980-01-01",
+          dateLabel: null as string | null,
+      }),
+      computed: {
+        dateFormDateLabel(): Date{
+            let label = this.defaultDateLabel
+            if (this.dateLabel ! == null) {
+                label = this.dateLabel
+            }
+            return new Date(label)
+        }
+      }
+  })
+</script>
+
+<style scoped>
+
+</style>
